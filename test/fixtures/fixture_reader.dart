16f572bee9949c568d0a532d49437c9bce9ca122
Ville-Matti Hakanpää <ville-matti.hakanpaa@futuriot.com>
2020-02-26 23:00:14 +0200

    Put try/catch to fixture_reader

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -1,3 +1,12 @@
 import 'dart:io';
 
-String fixture(String name) => File('test/fixtures/$name').readAsStringSync();
+String fixture(String name) {
+  String jsonString;
+  try {
+    jsonString = File('test/fixtures/$name').readAsStringSync();
+    return jsonString;
+  } catch (e) {
+    jsonString = File('../test/fixtures/$name').readAsStringSync();
+    return jsonString;
+  }
+}
